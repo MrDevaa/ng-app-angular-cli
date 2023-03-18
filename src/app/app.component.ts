2bc59ee269dfd414d577653eca03cdd3b036c524
MrDevaa <d.codandabany@gmail.com>
2023-03-18 23:37:42 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: '/app.component.html'
+})
+export class AppComponent implements OnInit {
+  pokemonList : Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined; 
+  
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    
+    if(pokemon) {
+      console.log(`vous avez cliquer sur le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+
+    }else {
+      console.log(`vous avez demandé un pokemon qui n'exsiste pas `);
+      this.pokemonSelected = pokemon;
+
+    }
+    
+  }
+
+}
